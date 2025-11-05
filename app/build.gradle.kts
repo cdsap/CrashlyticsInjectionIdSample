1f303e264ce7937faf3b3a724e1d465617f5a485
Inaki Villar <inaki.seri@gmail.com>
2025-11-05 18:41:20 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.firebase.crashlytics")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+
+        }
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":mylibrary"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of("17"))
+    }
+}
+
+androidComponents {
+    onVariants(selector = selector().all()) {
+        it.configureJavaCompileTask {
+            println("1"+ it.path)
+        }
+        it.nestedComponents.forEach {
+            it.configureJavaCompileTask {
+                println("2"+it.path)
+            }
+        }
+    }
+}
+
---
+++ mylibrary/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.example.mylibrary"
+    compileSdk = 36
+    buildFeatures {
+        dataBinding = true
+    }
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of("17"))
+    }
+}
+tasks.withType<JavaCompile>().configureEach {
+    javaCompiler = javaToolchains.compilerFor(java.toolchain)
+}
