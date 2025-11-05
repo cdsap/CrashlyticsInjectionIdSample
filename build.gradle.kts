3afd135bde9476aef742c2c444efc3c93af043fb
Inaki Villar <inaki.seri@gmail.com>
2025-11-05 22:56:45 +0100

    A

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
-    id("com.google.firebase.crashlytics") version "3.0.6" apply false
+    id("com.google.firebase.crashlytics") version "3.0.1" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 
 }
