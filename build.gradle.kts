8b53c8b20de336de9341459d847db5036a882f70
Inaki Villar <inaki.seri@gmail.com>
2025-11-10 08:59:18 -0800

    adding the correct version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
-    id("io.github.cdsap.gradleprocess") version "0.3.1-SNAPSHOT"
+    id("io.github.cdsap.gradleprocess") version "0.3.0"
    //     id("org.gradle.android.cache-fix") version "3.0.4-SNAPSHOT" apply false
 
 }
